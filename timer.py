0e6aa9ef8aaa984c4f5b60eab10703812db47056
balakshinphil <balakshinphil@gmail.com>
2020-05-12 16:29:06 +0300

    some refactoring

--- timer.py
+++ timer.py
@@ -4,24 +4,29 @@ from time import sleep
 
 class Timer():
     time = 0
+    
 
 
     def __init__(self, hours, minutes, seconds):
         self.time = hours * 3600 + minutes * 60 + seconds
 
 
-    def __printTime(self):
+    def __clean_line(self):
+        print('\b' * 12 + ' ' * 12 + '\r', end='')
+
+
+    def __print_time(self):
         hours = self.time // 3600
         minutes = (self.time - hours * 3600) // 60
         seconds = self.time - hours * 3600 - minutes * 60
-        print('\b' * 12 + ' ' * 12 + '\r', end='')
+        self.__clean_line()
         print(str(hours) + " : " + str(minutes) + " : " + str(seconds), end='')
         
 
     def run(self):
         while(self.time > 0):
-            self.__printTime()
+            self.__print_time()
             self.time -= 1
             sleep(1)
-        print('\b' * 12 + ' ' * 12 + '\r', end='')    
+        self.__clean_line()    
         print('0 : 0 : 0')
